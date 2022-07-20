510cc0cf413e64da9ef6dedf7bc91b62f7a8db38
JohnRudolfsson <jet0001@hotmail.com>
2022-07-20 15:01:27 -0300

    Imgs added

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+function removeTransition(e){
+    if (e.propertyName !== 'transform') {
+        return;
+    } else {
+        this.classList.remove('btn-clicked');
+    }
+    console.log(e);
+}
+
 const btns = document.querySelectorAll('.btn');
 btns.forEach(btn => {
     btn.addEventListener('click', () =>{
-        
+        btn.classList.add('btn-clicked');
+        btn.addEventListener('transitionend', removeTransition);
     })
+    
 });
+
+
